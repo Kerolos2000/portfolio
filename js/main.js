edeb7f4c7fcbdab65988e9daff82a706816d20cd
kerolos <kero.magdy2000@gmail.com>
2022-12-03 19:53:59 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,162 @@
+// skills api
+let skillsX = document.querySelector("#skillsX");
+fetch("../skillsJSON/skills.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    for (let i = 0; i < response.length; i++) {
+      skillsX.innerHTML += `
+        <div class="card col-xl-4 col-md-6 col-sm-6 col-6" data-aos="zoom-in-down" data-aos-duration="1000">
+            <div class="cardX1">
+            <div class="front">
+                <div class="card-img">
+                <i class="${response[i].sClass} fa-fw"></i>
+                </div>
+                <div class="card-bodr">
+                <h3>${response[i].sName}</h3>
+                </div>
+            </div>
+            <div class="back">
+                <label for="skillpro">${response[i].sPercent}%</label>
+                <div id="skillpro" class="progress aos-init aos-animate" data-aos="fade-right">
+                <div class="progress-bar"
+                    role="progressbar" style="width: ${response[i].sPercent}%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            </div>
+        </div>
+        `;
+    }
+  });
+
+// loaader
+let loader = document.getElementById("loader");
+let NAV = document.getElementById("NAV");
+let toUp = document.querySelector("#toUp");
+document.body.style.maxHeight = "100vh";
+document.body.style.overflow = "hidden";
+setTimeout(loaderNone, 2000);
+function loaderNone() {
+  document.body.style.maxHeight = "auto";
+  document.body.style.overflow = "auto";
+  loader.style.zIndex = "-1";
+  loader.style.opacity = "0";
+  NAV.style.opacity = "1";
+  toUp.style.zIndex = "99999999";
+}
+
+// github api
+gitID = document.querySelector("#gitID");
+fetch("https://api.github.com/users/kerolos2000/repos")
+  .then((response) => response.json())
+  .then((response) => {
+    // console.log(response);
+    for (let i = 0; i < response.length; i++) {
+      gitID.innerHTML += `
+		<div id="card3d"  class="card col-xl-4 col-md-6 col-sm-6 col-6" data-aos="zoom-in-down" data-aos-duration="1000">
+		<div class="cardX">
+		  <div class="front">
+			<div class="card-body">
+			  <h3>${response[i].name}</h3>
+			  <h6>NO. of Stars ${response[i].stargazers_count}<i class="fa-regular fa-star"></i></h6>
+			  <div class="mb-3">
+				<h5>Show code</h5>
+				<a id="mouse-cursor-gradient-tracking" class="btn visit" target="_blank" href="${response[i].html_url}">Show Now</a> 
+			  </div>
+			  <div class="mt-3 mb-2">
+			  	<h5>Show live demo</h5>
+			  <a id="mouse-cursor-gradient-tracking" class="btn button-text visit" target="_blank" href="https://kerolos2000.github.io/${response[i].name}">Live Demo</a>	   
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+		`;
+      let card3d = document.querySelectorAll("#card3d");
+      let cardX = document.querySelectorAll(".cardX");
+      for (let i = 0; i < card3d.length; i++) {
+        card3d[i].onmousemove = function (e) {
+          let half = cardX[i].offsetHeight / 2;
+          cardX[i].style.transform = `rotateX(${
+            -(e.offsetY - half) / 10
+          }deg) rotateY(${(e.offsetX - half) / 10}deg)`;
+        };
+        card3d[i].onmouseleave = function () {
+          cardX[i].style.transform = `rotateX(0deg) rotateY(0deg)`;
+        };
+      }
+
+      let btn = document.querySelectorAll("#mouse-cursor-gradient-tracking");
+      for (let j = 0; j < btn.length; j++) {
+        btn[j].addEventListener("mousemove", (mouse) => {
+          let rect = mouse.target.getBoundingClientRect();
+          let x = mouse.clientX - rect.left;
+          let y = mouse.clientY - rect.top;
+          btn[j].style.setProperty("--x", x + "px");
+          btn[j].style.setProperty("--y", y + "px");
+        });
+      }
+    }
+  });
+
+// to up
+window.onscroll = function () {
+  if (window.scrollY > 100) {
+    toUp.style.display = "block";
+    toUp.addEventListener("click", function () {
+      window.scrollTo(0, 0);
+    });
+  } else {
+    toUp.style.display = "none";
+  }
+  sessionStorage.setItem("scrollY", window.scrollY);
+};
+window.scrollTo(0, sessionStorage.getItem("scrollY"));
+
+// form validation
+(() => {
+  "use strict";
+  const forms = document.querySelectorAll(".needs-validation");
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }else{
+          Swal.fire(
+            'Done!',
+            'Your message has been sent successfully',
+            'success'
+            )
+          }
+          form.classList.add("was-validated");
+          event.preventDefault();
+      },
+      false
+    );
+  });
+})();
+
+
+
+// preventDefault 
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+  // if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+  //   return false;
+  // }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
